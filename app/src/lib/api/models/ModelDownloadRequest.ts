ef8cbde34d447dfed94f2eda548f973860fd4c41
dothicdiu <7978939+dothicdiu@users.noreply.github.com>
2026-01-25 04:57:55 -0800

    api

---
+++ app/src/lib/api/models/ModelDownloadRequest.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+/**
+ * Request model for triggering model download.
+ */
+export type ModelDownloadRequest = {
+    model_name: string;
+};
+
